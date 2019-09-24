e873cd9cae183c0ebaea63cf0de0552381b5d0e6
Bot Support <bot.support@emplay.net>
2019-09-23 18:18:00 -0700

    tic-tac-toe done

--- src/index.js
+++ src/index.js
@@ -29,6 +29,10 @@ class Board extends React.Component {
 
     // another way is to do the computation and then call setState
     const squares = [...this.state.squares];
+    // return early if square is already filled or if game over
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares,
@@ -43,7 +47,12 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    let status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    if (winner) {
+      status = `Game Over! '${winner}' has won!`
+    }
+    
 
     return (
       <div>
@@ -66,6 +75,26 @@ class Board extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 class Game extends React.Component {
